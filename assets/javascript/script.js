3af317c68fdf9e7ffa2ba719c62d315fea6bce3a
Black Jack <hibbarcd@gmail.com>
2019-06-03 14:40:48 -0500

    overlays and polish of UI

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,45 +3,50 @@ $(document).ready(function() {
 
     var random = Math.floor(Math.random()*(120-19)+19);
     $("#numberToGet").text(random);
-    
-    var num1, num2, num3, num4 = Math.floor(Math.random()*(12-1)+1);
-    
+        
     var userTotal= 0;
-    var wins = 0;
+    var wins =  0;
     var losses = 0;
     
     $("#numberWins").text(wins);
     $("#numberLosses").text(losses);
-    
+
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            // reset();
+        });
+    });
     function reset() {
+    // Generate random number value for target number
         random = Math.floor(Math.random()*(120-19)+19);
-        console.log(random);
+        // console.log(random);
         $("#numberToGet").text(random);
-        var num1, num2, num3, num4 = Math.floor(Math.random()*(12-1)+1);
 
         userTotal = 0;
         $("#score").text(userTotal);
-       
-    } 
+       } 
+
     function winner() {
-        alert("You Won!!");
         wins++;
         $("#numberWins").text(wins);
+        document.getElementById('victory-text').classList.add('visible');
         reset();
     }
     function loser() {
-        alert("You Lose!!");
         losses++;
         $("#numberLosses").text(losses);
+        document.getElementById('game-over-text').classList.add('visible');
         reset();
     }
+//Create array of values for crystals 
     var crystalValues = [];
  
     for (i = 0; i < 4; i++) { 
         crystalValues.push( Math.floor(Math.random()*(12-1)+1));
 
     }
-
     function getCrystalHandler(crystalKey) {
        return function() {
         userTotal = userTotal + crystalValues[crystalKey];
@@ -51,10 +56,12 @@ $(document).ready(function() {
     
         if (userTotal === random) {
             winner()
+            console.log(winner);
         }
     
         else if (userTotal > random) {
             loser()
+            console.log(loser);
         } 
     }
     }
